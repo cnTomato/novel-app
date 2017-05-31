5024189de4dfb36f73536967d36d01ecb23ac94f
beyourhero <gemini0525@foxmail.com>
2017-05-31 18:11:17 +0800

    vue分支

---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="index">
+        <h1>搜小说-拒绝广告</h1>
+        <div class="searchArea">
+            <input type="text">
+            <i class="icon icon-search"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                searchData: ''
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    html,body,#app,.index{
+        width:100%;
+        height:100%;
+    }
+</style>
